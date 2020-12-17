5d7932d8d17d6ea7f1657d57ac5469ba6ad856ff
Bilal Bayasut <bilalbayasut@gmail.com>
2020-12-17 15:17:02 +0700

    fix apply input and output issue

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This repo containing Terraform configuration files to provision an EKS cluster o
 
 put your cluster name and region
 aws eks update-kubeconfig --name getting-started-eks --region ap-southeast-2
+
+# note
+
+if your kubernetes cluster create a service, it triggers classic load balancer on AWS and it's not mapped by terraform,
+thus, this must be noted and deleted first manually (at least for now)
--- staging/providers.tf
+++ staging/providers.tf
@@ -3,9 +3,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "helm" {
-  # Configuration options
-}
+# provider "helm" {
+#   # Configuration options
+#   # kubernetes {
+#   #   host                   = data.aws_eks_cluster.cluster.endpoint
+#   #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   #   token                  = data.aws_eks_cluster_auth.cluster.token
+#   #   load_config_file       = false
+#   # }
+# }
 
 data "aws_eks_cluster" "cluster" {
   name = module.k8s.cluster_id
@@ -15,9 +21,9 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.k8s.cluster_id
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   load_config_file       = false
+# }
